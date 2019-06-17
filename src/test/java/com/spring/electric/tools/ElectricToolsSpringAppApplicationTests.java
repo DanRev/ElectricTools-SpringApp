fb1f57647d5d12e6dd9d39169f94af70f40a8c40
DanielParra <danielparra05@outlook.com>
2019-06-17 00:07:44 -0500

    Adicion de primera prueba unitaria

--- src/test/java/com/spring/electric/tools/ElectricToolsSpringAppApplicationTests.java
+++ src/test/java/com/spring/electric/tools/ElectricToolsSpringAppApplicationTests.java
@@ -1,16 +1,48 @@
 package com.spring.electric.tools;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 import org.junit.Test;
+
+
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.spring.electric.tools.models.dao.IClienteDAO;
+import com.spring.electric.tools.models.entity.Cliente;
+
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
 public class ElectricToolsSpringAppApplicationTests {
 
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Autowired
+	private IClienteDAO clienteRepo;
+
+	// write test cases here
+	
 	@Test
-	public void contextLoads() {
+	public void testInsertOrden() {
+		 // given
+	    Cliente dani = new Cliente();
+	    dani.setNombre("dani");
+	    dani.setApellido("parra");
+	    dani.setCedula("1094970787");
+	    entityManager.persist(dani);
+	    entityManager.flush();
+	 
+	    // when
+	    Cliente found = clienteRepo.findByCedula(dani.getCedula());
+	 
+	    // then
+	    assertThat(found.getCedula()).isEqualTo(dani.getCedula());
 	}
 
 }
